af739479e3d5d43c2856e172b7e72e632ddf4fb9
SrGonz4gga <163474818+SrGonz4gga@users.noreply.github.com>
2024-06-17 19:52:29 -0300

    exercicio6.3

--- modulo6/aula3_questao3.py
+++ modulo6/aula3_questao3.py
@@ -1,10 +1,23 @@
-from random import randint
+import random
+lista_numeros = [random.randint(-10, 10) for _ in range(20)]
+print("Lista original:", lista_numeros)
 
-lista = []
-for i in range (20):
-    lista.append(randint(-10,10))
+maior_negativos = None
+inicio_negativos = None
+contagem_negativos = 0
 
-for n in lista:
-    
+for i, numero in enumerate(lista_numeros):
+    if numero < 0:
+        if inicio_negativos is None:
+            inicio_negativos = i
+        contagem_negativos += 1
+    else:
+        if contagem_negativos > 0:
+            if maior_negativos is None or contagem_negativos > maior_negativos:
+                maior_negativos = contagem_negativos
+                fim_negativos = i - 1
 
-print(lista)
+        contagem_negativos = 0
+if maior_negativos is not None:
+    del lista_numeros[inicio_negativos:fim_negativos + 1]
+print("Lista final:", lista_numeros)
